cc734287c3f22673cecc182d8c7234f2d2970639
Konstantin Kharlambov <kostharl@gmail.com>
2018-01-21 02:29:32 +0200

    Added a stub controller

---
+++ TheOxygenGame/Controller/GameBoardController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheOxygenGame.Model;
+
+namespace TheOxygenGame.Controller
+{
+    public class GameBoardController
+    {
+        private IGameBoardModel<int> _gameBoard;
+
+        public GameBoardController(IGameBoardModel<int> gameBoard)
+        {
+            _gameBoard = gameBoard;            
+        }
+
+        public void GameKeyPressed()
+        {
+            throw new NotImplementedException();
+        }        
+    }
+}
